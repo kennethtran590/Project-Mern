aa19b730f3771b31504ec227a4ef294824133dfc
Kenneth Tran <48599140+kennethtran590@users.noreply.github.com>
2022-09-04 16:46:01 -0700

    Initialize and connect MongoDB

--- server/index.js
+++ server/index.js
@@ -1,12 +1,25 @@
 import express from "express";
-const port = 3001;
+import mongoose from "mongoose";
+import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("Hello Express");
-});
+app.use(morgan("dev"));
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+const MONGODB_URL =
+  "mongodb+srv://kenneth:kenny59048@cluster0.kuzr0zg.mongodb.net/tour-db?retryWrites=true&w=majority";
+
+const port = 3001;
+
+mongoose
+  .connect(MONGODB_URL)
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server running on port ${port}`);
+    });
+  })
+  .catch((error) => console.log(`${error} did not connect`));
